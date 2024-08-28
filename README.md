c9d51aa9cab85dc7a7069b8f28659bd4a9e4aca0
Marcosrdsz <rdszmarcos@gmail.com>
2024-08-28 19:34:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# projetos
+ projetos que eu desenvolvi
---
+++ projeto curso em video 02/my.js
@@ -0,0 +1,4 @@
+var n1 = Number.parseInt(window.prompt("digite um numero"))
+var n2 = Number.parseInt(window.prompt("digite outro numero"))
+var resultado = n1 + n2
+window.alert('o resultado é: '+ resultado)
